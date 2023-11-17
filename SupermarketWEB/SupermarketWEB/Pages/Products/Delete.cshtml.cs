753b3bffa55de828364414404a9586e7d8a15968
zarkprogame <zarkprogame@gmail.com>
2023-11-17 10:36:55 -0500

    Delete Product

---
+++ SupermarketWEB/SupermarketWEB/Pages/Products/Delete.cshtml.cs
@@ -0,0 +1,52 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using SupermarketWEB.Data;
+using SupermarketWEB.Models;
+
+namespace SupermarketWEB.Pages.Products
+{
+    public class DeleteModel : PageModel
+    {
+		private readonly SupermarketContext _context;
+		public DeleteModel(SupermarketContext context)
+		{
+			_context = context;
+		}
+
+		[BindProperty]
+		public Product Products { get; set; } = default!;
+		public async Task<IActionResult> OnGetAsync(int? id)
+		{
+			if (id == null || _context.Products == null)
+			{
+				return NotFound();
+			}
+			var product = await _context.Products.FirstOrDefaultAsync(m => m.Id == id);
+
+			if (product == null)
+			{
+				return NotFound();
+			}
+			else {
+				Products = product;
+			}
+			return Page();
+		}
+		public async Task<IActionResult> OnPostAsync(int? id)
+		{
+			if (id == null || _context.Products == null)
+			{
+				return NotFound();
+			}
+			var product = await _context.Products.FindAsync(id);
+			if (product != null)
+			{
+				Products = product;
+				_context.Products.Remove(Products);
+				await _context.SaveChangesAsync();
+			}
+			return RedirectToPage("./Index");
+		}
+	}
+}
